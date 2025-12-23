def58f6448cf75f2d38d6a0c048675bee3f8de7b
bala-amypo <bala@amypo.in>
2025-12-23 05:24:59 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ApiUsageLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ApiUsageLogServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.dto.ApiUsageLogDto;
 import com.example.demo.model.ApiKey;
 import com.example.demo.model.ApiUsageLog;
 import com.example.demo.repository.ApiUsageLogRepository;
 import com.example.demo.service.ApiUsageLogService;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class ApiUsageLogServiceImpl implements ApiUsageLogService {
@@ -15,19 +19,33 @@ public class ApiUsageLogServiceImpl implements ApiUsageLogService {
         this.apiUsageLogRepository = apiUsageLogRepository;
     }
 
-    
+    /**
+     * REQUIRED by interface
+     */
     @Override
     public Long countRequestsToday(Long apiKeyId) {
         return 0L;
     }
 
-
+    /**
+     * REQUIRED by interface
+     */
     @Override
-    public Long getUsageForToday(Long apiKeyId) {
-        return 0L;
+    public List<ApiUsageLogDto> getUsageForToday(Long apiKeyId) {
+        return Collections.emptyList();
     }
 
+    /**
+     * REQUIRED by interface
+     */
+    @Override
+    public List<ApiUsageLogDto> getUsageForApiKey(Long apiKeyId) {
+        return Collections.emptyList();
+    }
 
+    /**
+     * REQUIRED by interface
+     */
     @Override
     public void logRequest(ApiKey apiKey, String endpoint) {
         ApiUsageLog log = new ApiUsageLog();
